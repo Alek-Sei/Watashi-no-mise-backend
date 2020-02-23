a19975db9452c231bc8339f4b020f0c4ab4f76f0
Aleksei Skokov <a@1x.ee>
2020-02-23 21:32:33 +0200

    Initial Commit - Boilerplate

---
+++ src/main/java/com/onex/watashinomise/WatashinomiseApplication.java
@@ -0,0 +1,13 @@
+package com.onex.watashinomise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatashinomiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatashinomiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onex/watashinomise/WatashinomiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onex.watashinomise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatashinomiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
