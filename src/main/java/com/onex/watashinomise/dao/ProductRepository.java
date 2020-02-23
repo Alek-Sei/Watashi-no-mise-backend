4cf09e306d54b66958a71f32e7f4955f322d64cc
Aleksei Skokov <a@1x.ee>
2020-02-23 22:25:16 +0200

    Added 2 repos: Product & Product Category

---
+++ src/main/java/com/onex/watashinomise/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.onex.watashinomise.dao;
+
+import com.onex.watashinomise.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
